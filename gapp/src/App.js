ccde87df6d5b4a1c995106640a3b5eab9570cddd
ylech <waltermelonson@gmail.com>
2019-07-01 08:53:53 -0700

    layout for search page complete tested with json file for results. Properly displays all results to page with styling

--- gapp/src/App.js
+++ gapp/src/App.js
@@ -1,33 +1,48 @@
-import React from 'react';
-import './App.css';
-import Form from "./Components/Form"
-import Navbar from "./Components/Navbar"
-import Row from "./Components/Row"
-import Col from "./Components/Col"
-import Display from "./Components/Display"
-import Title from "./Components/Title"
-import Results from "./Components/Results"
+import React, { Component } from "react";
+import "./App.css";
+import Form from "./Components/Form";
+import Navbar from "./Components/Navbar";
+import Row from "./Components/Row";
+import Col from "./Components/Col";
+import Display from "./Components/Display";
+import Title from "./Components/Title";
+import Results from "./Components/Results";
+import json from "./test.json";
 
-function App() {
-  return (
-    <div className="App container">
+class App extends Component {
+  state = {
+    json
+  };
+
+  render() {
+    return (
+      <div className="App container">
         <Navbar />
         <Title />
         <Row>
           <Col width="col-12">
-             <Form />
+            <Form />
           </Col>
         </Row>
         <Row>
           <Col width="col-12">
             <Results>
-              <Display />
+              {this.state.json.map(item => (
+                <Display
+                  key={item.id}
+                  title={item.title}
+                  subtitle={item.subtitle}
+                  author={item.author}
+                  url={item.url}
+                  summary={item.summary}
+                />
+              ))}
             </Results>
           </Col>
         </Row>
-        
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
--- gapp/src/Components/Display.js
+++ gapp/src/Components/Display.js
@@ -1,21 +1,36 @@
 import React from "react";
-import "./displayStyle.css"
+import "./displayStyle.css";
 
-function Display (props) {
-    return (
-        <div className="card mt-5">
-            <div className="row">
-                <div className="card-title">
-                    yes
-                </div>
-                <button className="btn btn-warning">View</button>
-                <button className="btn btn-info">Save</button>
-            </div>
-            <div className="row">
-                <img className="" src="" alt="" />
-            </div>
+function Display(props) {
+  return (
+    <div className="card shadow-lg mt-5">
+      <div className="row cardTop">
+        <div className="col-9">
+          <h3 className="card-title">{props.title}</h3>
         </div>
-    )
+        <div className="col-3">
+          <button className="btn btn-warning btn-sm mr-3 shadow">View</button>
+          <button className="btn btn-info btn-sm shadow">Save</button>
+        </div>
+      </div>
+      <div className="row">
+        <h5 className="col-12 sub">{props.subtitle}</h5>
+      </div>
+      <div className="row">
+        <h5 className="col-12 sub">{props.author}</h5>
+      </div>
+      <div className="row">
+        <div className="card-body">
+          <div className="col-3 image">
+            <img className="" src={props.url} alt="" />
+          </div>
+          <div className="col-9 sum">
+            <p>{props.summary}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Display;
