b00254f982439fa19b1b452846d5085f129cdc70
h f <waltermelonson@gmail.com>
2019-07-10 22:15:55 -0700

    Cleaning up code

--- gapp/src/pages/books.js
+++ gapp/src/pages/books.js
@@ -4,12 +4,10 @@ import Col from "../Components/Col";
 import MyDisplay from "../Components/MyDisplay";
 import Title from "../Components/Title";
 import Results from "../Components/Results";
-import json from "../test.json";
 import API from "../utils/API";
 
 class Search extends Component {
   state = {
-    json,
     myBooks: [],
     id: ""
   };
@@ -17,36 +15,26 @@ class Search extends Component {
   populate = () => {
     API.grabBooks()
       .then(data => {
-        console.log(data);
         this.setState({ myBooks: data.data });
       })
       .catch(err => console.log(err));
   };
 
   componentDidMount() {
-    console.log("mounted");
     this.populate();
   }
 
   removeBook = event => {
-    // console.log("deleted")
+    
     const ident = event.target.id;
     var newBook = this.state.myBooks.findIndex(function(book, i) {
-        console.log(book)
       return book._id === ident;
     });
 
-
-
-    // this.setState({myBooks: this.state.myBooks.filter(book => this.state.myBooks.indexOf(book) !== newBook )})
     const book = event.target.id;
     var newId = book
     this.setState({id: newId})
-    console.log(book);
-    console.log(this.state.myBooks[newBook].title);
-    console.log(newBook)
     const testBook = this.state.myBooks[newBook].title;
-    console.log(testBook)
     API.deleteBook(testBook).then(() => {
       this.populate();
     });
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+//Import block for dependencies
 const express = require("express");
-
 const mongoose = require("mongoose");
 const routes =require("./routes")
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//Established middleware preferences and for when to use production parameters
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 if (process.env.NODE_ENV === "production") {
@@ -12,10 +13,12 @@ if (process.env.NODE_ENV === "production") {
 }
 app.use(routes);
 
+//Creates the connection to the mongoose db
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
     useNewUrlParser: true 
 })
 
+//Begins listening
 app.listen(PORT, function(){
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
 })
